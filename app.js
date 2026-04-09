5bab23ccf7fea5ca59d0cc659fdce0f854b73f01
zullytamayom <zullytamayom@gmail.com>
2026-04-09 15:20:10 -0500

    agrego funcion de sumar precios

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const badge = document.getElementById('badge');
 const botones = document.querySelectorAll('.btn-agregar');
 
 botones.forEach(function(boton) {
-
   boton.addEventListener('click', function() {
 
     let cantidad = parseInt(badge.textContent);
@@ -12,3 +11,30 @@ botones.forEach(function(boton) {
   });
 
 });
+
+function mostrarTotal(total) {
+  const elemento = document.getElementById('total');
+
+  if (elemento) {
+    elemento.textContent =
+      '$' + total.toLocaleString('es-CO');
+  }
+}
+
+let total = 0;
+
+botones.forEach(function(boton) {
+
+  boton.addEventListener('click', function() {
+
+    const precio = Number(this.dataset.precio);
+
+    total += precio;
+
+    mostrarTotal(total);
+
+  });
+
+});
+
+
